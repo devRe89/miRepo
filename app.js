83278269b21f8667132949dde86c8e981949734e
devRe89 <develop.r89@gmail.com>
2021-09-22 11:52:27 -0300

    subiendo repo

---
+++ app.js
@@ -0,0 +1,35 @@
+
+var r = [{id:"1", comida: "Pizza"}, 
+		 {id:"2", comida:"Arroz"}, 
+		 {id:"3", comida:"Arepa"},
+		 {id:"3", comida:"Pasta"}
+        ];
+var u = [{id:"1",nombre:"Reni"},
+        {id:"2",nombre:"Michel"},
+        {id:"3",nombre:"Jorge"}];
+
+const indexByKey = (arr, key) => arr.reduce((acc, el) => {
+    if(!acc[el[key]]){
+        acc[el[key]] = []
+        acc[el[key]].push(el)
+    }else{
+        acc[el[key]].push(el);
+    }
+  return acc
+}, {});
+
+var oi = indexByKey(r, 'id');
+// console.log(oi);
+const res = [] ;
+for (const iterator of u) {
+    res.push({
+        info: iterator,
+        accion: oi[iterator.id]
+    });
+}
+
+console.log(JSON.stringify(res,null,'\t'));
+
+
+
+
